21ed8ef37c5d6139ca19cf836d95b1e381e63284
Rahguzar <aikrahguzar@zohomail.eu>
2022-09-08 13:36:16 +0200

    A little refactor for sage blocks

--- sage-blocks.el
+++ sage-blocks.el
@@ -49,30 +49,20 @@ Note that '^' to match at the beginning of the line should not be added to
 (defun sage-blocks-backward (arg)
   "Move backwards to the ARGth previous beginning of a block."
   (interactive "p")
-  (if (< arg 0)
-      (sage-blocks-forward (- arg))
-    (while (and (> arg 0)
-                (search-backward-regexp (concat "^" sage-blocks-delimiter) nil 'move))
-      (setq arg (- arg 1)))))
+  (sage-blocks-forward (- arg)))
 
 ;;;###autoload
 (defun sage-blocks-forward (arg)
   "Move forwards to the next ARGth beginning of a block."
   (interactive "p")
-  (if (< arg 0)
-      (sage-blocks-backward (- arg))
     ;; If point is on a delimiter, we should skip this, so search from beginning of
     ;; next line (this will match immediately, if next line is a delimiter)
-    (let ((re  (concat "^" sage-blocks-delimiter)))
+    (let ((re  (rx bol (literal sage-blocks-delimiter))))
       (when (looking-at re)
-        (forward-line))
+        (forward-line (if (> arg 0) 1 -1)))
       ;; search forward: if it worked, move to begin of delimiter, otherwise end of file
-      (while (and (> arg 0)
-                  (search-forward-regexp re nil 'move))
-        (setq arg (- arg 1)))
-      ;; We successfully found something so move to the beginning of the match
-      (when (= arg 0)
-        (goto-char (match-beginning 0))))))
+      (when (re-search-forward re nil t arg)
+        (goto-char (match-beginning 0)))))
 
 ;;;###autoload
 (defun sage-blocks-send-current ()
@@ -86,19 +76,16 @@ Move to end of block sent."
     (python-shell-send-region (point)
                               (progn (sage-blocks-backward 1) (point)))))
 
-
 (defun sage-blocks-find-all ()
   "Find all the blocks in the current buffer."
   (save-excursion
     (goto-char (point-min))
-    (let ((blocks nil)
-          (pos nil))
-      (while (setq pos (re-search-forward
-                        (rx line-start (literal sage-blocks-delimiter))
-                        nil t))
-        (goto-char pos)
+    (let ((blocks nil))
+      (while (re-search-forward
+              (rx line-start (literal sage-blocks-delimiter))
+              nil t)
         (push (string-trim (thing-at-point 'line t)
-                           (rx (0+ (or space (literal sage-blocks-delimiter)))))
+                           (rx (1+ (or space (literal sage-blocks-delimiter)))))
               blocks))
       blocks)))
 
@@ -106,13 +93,13 @@ Move to end of block sent."
 (defun sage-blocks-goto (block)
   "Goto the named BLOCK from the current buffer using completion."
   (interactive
-   (list (completing-read "Send block: " (sage-blocks-find-all) nil t)))
+   (list (completing-read "Go to block: " (sage-blocks-find-all) nil t)))
   (when-let (pos (save-excursion
                    (goto-char (point-min))
                    (re-search-forward
                     (rx line-start (literal sage-blocks-delimiter)
                         (0+ space) (literal block)))))
-    (goto-char pos)))
+    (goto-char (match-beginning 0))))
 
 ;;;###autoload
 (defun sage-blocks-send (block)
